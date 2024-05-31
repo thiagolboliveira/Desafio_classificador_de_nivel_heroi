84b344b5061ae846b1a879570897e7abb9d3af93
thiagolboliveira <thiago.oliveira_corporation@hotmail.com>
2024-05-31 11:57:29 -0300

    Desafio Concluído
    Desafio finalizado!

---
+++ index.js
@@ -0,0 +1,9 @@
+//const entrada = require('prompt-sync')({sigint: true});//
+let nivel;
+let name;
+let continua;
+
+window.alert('Bem Vindo!')
+window.prompt('Qual é o seu nome?')
+window.confirm("Você deseja avaliar seu nível ? Digite 'sim' para continuar ou 'não' para sair: ");
+
